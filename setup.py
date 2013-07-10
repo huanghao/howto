83fa401b785e6d3f98f4955c4fdb3f3f52a71d83
huanghao <huang1hao@gmail.com>
2013-07-10 23:05:22 +0800

    Add howto.py and setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#from distribute_setup import use_setuptools
+#use_setuptools()
+
+from setuptools import setup
+
+setup(name='howto',
+      scripts=['howto.py'],
+      install_requires=['distribute'],
+      )
